8a58399d51413380570544e8fa6239a6c5436d8e
fiorenzo pizza <fiorenzino@gmail.com>
2015-05-22 10:45:47 +0200

    upload

--- fiorenzopizza/js-private/app/scripts/controllers/upload.js
+++ fiorenzopizza/js-private/app/scripts/controllers/upload.js
@@ -4,30 +4,36 @@
 
 angular.module('giavacms-private')
 
-  .service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function (file, uploadUrl, name, description) {
-      var fd = new FormData();
-      fd.append("name", name);
-      fd.append("description", description);
-      fd.append('file', file);
-      $http.post(uploadUrl, fd, {
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-      })
-        .success(function () {
-        })
-        .error(function () {
-        });
-    }
-  }])
+    .service('siglefileUpload', ['$http', function ($http) {
+        this.uploadFileToUrl = function (uploadUrl, fileObj) {
+            var fd = new FormData();
+            angular.forEach(fileObj, function (key, value) {
+                fd.append(key, value);
+            });
+            //fd.append("name", name);
+            //fd.append("description", description);
+            //fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            })
+                .success(function () {
+                })
+                .error(function () {
+                });
+        }
+    }])
 
-  .controller('myCtrl', ['$scope', 'fileUpload', function ($scope, fileUpload) {
+    .controller('UploadController', ['$scope', 'fileUpload', function ($scope, fileUpload) {
 
-    $scope.uploadFile = function () {
-      var file = $scope.myFile;
-      console.log('file is ' + JSON.stringify(file));
-      var uploadUrl = "http://localhost:8080/api/v1/richcontent/1-agosto-ore-2130--le-marche-i-manicomi-i-matti-gli-amori/image";
-      fileUpload.uploadFileToUrl(file, uploadUrl, $scope.name, $scope.description);
-    };
+        $scope.uploadFile = function () {
+            console.log('file is ' + JSON.stringify(file));
+            var uploadUrl = "http://localhost:8080/api/v1/richcontent/1-agosto-ore-2130--le-marche-i-manicomi-i-matti-gli-amori/image";
+            var fileObj = {};
+            fileObj.name = $scope.name;
+            fileObj.description = $scope.description;
+            fileObj.file = $scope.myFile;
+            fileUpload.uploadFileToUrl(uploadUrl, fileObj);
+        };
 
-  }]);
+    }]);
