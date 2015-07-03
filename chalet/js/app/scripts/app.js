2a022e0d5b8fcec4edcb35ccdf4875f96a51336b
fiorenzo pizza <fiorenzino@gmail.com>
2015-07-03 09:13:32 +0200

    tanta roba

---
+++ chalet/js/app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jsApp
+ * @description
+ * # jsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
