fd7975eefc612644ee7f6dba937c9a63c1cc0c76
fiorenzo pizza <fiorenzino@gmail.com>
2015-07-03 18:37:33 +0200

    merda per cristo

--- chalet/js/app/scripts/controllers/chalet.js
+++ chalet/js/app/scripts/controllers/chalet.js
@@ -2,41 +2,28 @@
 
 angular.module('jsApp')
 
-  .controller('Chalet', ['$scope', 'RsResource', 'APP_PROPERTIES', '$location', function ($scope, RsResource, APP_PROPERTIES, $location) {
-
-    var getReqParams = function () {
-      var reqParams = {};
-      reqParams['host'] = APP_PROPERTIES.HOST;
-      reqParams['context'] = APP_PROPERTIES.CONTEXT;
-      reqParams['entityPath'] = 'chalets';
-      return reqParams;
-    }
-
-    var reqParams = getReqParams();
-    reqParams['startRow'] = 0;
-    reqParams['pageSize'] = 0;
-    RsResource.query(reqParams, function (model) {
-      $scope.model = model;
-    });
-
-    $scope.go = function ( path ) {
-      $location.path( path );
-    };
-
-  }])
+    .controller('Chalet', ['$scope', 'ChaletService', function ($scope, ChaletService) {
 
 
-  .config(['$stateProvider', function ($stateProvider) {
+        ChaletService.getList({}, 0, 0, function (data) {
+            $scope.model = data;
+        });
 
-    $stateProvider.
-      state('chalet', {
-        url: '/chalet',
-        controller: 'Chalet',
-        templateUrl: 'views/chalet.html',
-        ncyBreadcrumb: {
-          label: 'chalet'
-        }
-      })
 
-  }])
+    }])
+
+
+    .config(['$stateProvider', function ($stateProvider) {
+
+        $stateProvider.
+            state('chalet', {
+                url: '/chalet',
+                controller: 'Chalet',
+                templateUrl: 'views/chalet.html',
+                ncyBreadcrumb: {
+                    label: 'chalet'
+                }
+            })
+
+    }])
 
