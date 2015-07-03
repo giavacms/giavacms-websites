cd45cdc87d95209e555ce39993e32bdfa2c2fcf0
fiorenzo pizza <fiorenzino@gmail.com>
2015-07-03 09:29:29 +0200

    tanta roba

--- chalet/js/app/scripts/app.js
+++ chalet/js/app/scripts/app.js
@@ -9,9 +9,61 @@
  * Main module of the application.
  */
 angular
-  .module('jsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngTouch'
-  ]);
+    .module('jsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ngTouch'
+    ])
+
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html',
+                ncyBreadcrumb: {
+                    label: 'Home page'
+                }
+            })
+
+            .state('chalet', {
+                url: '/chalet',
+                templateUrl: 'views/chalet.html',
+                ncyBreadcrumb: {
+                    label: 'chalet'
+                }
+            })
+
+            .state('chalet_single', {
+                url: '/chalet/:id',
+                templateUrl: 'views/chalet_single.html',
+                ncyBreadcrumb: {
+                    label: 'chalet single'
+                }
+            })
+
+
+            .state('classifica', {
+                url: '/classifica',
+                templateUrl: 'views/classifica.html',
+                ncyBreadcrumb: {
+                    label: 'classifica'
+                }
+            })
+
+            .state('vota', {
+                url: '/vota',
+                templateUrl: 'views/vota.html',
+                ncyBreadcrumb: {
+                    label: 'vota'
+                }
+            })
+
+
+    }])
+;
+
