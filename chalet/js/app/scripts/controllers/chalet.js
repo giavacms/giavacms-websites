1d43ff6903a40f8fa609479bc59bfccca6545555
fiorenzo pizza <fiorenzino@gmail.com>
2015-07-20 19:15:58 +0200

    ancora non ho metodo per invocare chalet singolo da licensenumber

--- chalet/js/app/scripts/controllers/chalet.js
+++ chalet/js/app/scripts/controllers/chalet.js
@@ -4,28 +4,6 @@ angular.module('jsApp')
 
   .controller('Chalet', ['$scope', 'ChaletService', '$filter', function ($scope, ChaletService, $filter) {
 
-    $scope.models = [];
-
-    ChaletService.getList({}, 0, 19).then(function (data) {
-      $scope.models[0] = data;
-    });
-    ChaletService.getList({}, 20, 39).then(function (data) {
-      $scope.models[1] = data;
-    });
-    ChaletService.getList({}, 40, 59).then(function (data) {
-      $scope.models[2] = data;
-    });
-    ChaletService.getList({}, 60, 79).then(function (data) {
-      $scope.models[3] = data;
-    });
-    ChaletService.getList({}, 80, 99).then(function (data) {
-      $scope.models[4] = data;
-    });
-    ChaletService.getList({}, 100, 119).then(function (data) {
-      $scope.models[5] = data;
-    });
-
-
     $scope.search = {};
     $scope.model = {};
     $scope.filteredModel = [];
