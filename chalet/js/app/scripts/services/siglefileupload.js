217c457bd1a7d83a6ed5233d827ca2bdb2fe7491
fiorenzo pizza <fiorenzino@gmail.com>
2015-07-31 16:47:42 +0200

    upload pezzi

---
+++ chalet/js/app/scripts/services/siglefileupload.js
@@ -0,0 +1,23 @@
+angular.module('jsApp')
+
+    .service('SigleFileUploadService', ['$http', function ($http) {
+        this.uploadFileToUrl = function (uploadUrl, fileObj) {
+            var fd = new FormData();
+            angular.forEach(fileObj, function (value, key) {
+                fd.append(key, value);
+            });
+            //fd.append("name", name);
+            //fd.append("description", description);
+            //fd.append('file', file);
+            var res = $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            });
+            res.success(function (data, status, headers, config) {
+                console.log(JSON.stringify(data));
+            });
+            res.error(function (data, status, headers, config) {
+                console.log(JSON.stringify(data));
+            });
+        }
+    }]);
