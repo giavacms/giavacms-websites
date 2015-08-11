5a4f529c72e5c53e1dcb3fbb7128b89fe476dceb
fiorenzo pizza <fiorenzino@gmail.com>
2015-08-11 22:06:52 +0200

    piccole cose

--- chalet/js/app/scripts/constants/global.js
+++ chalet/js/app/scripts/constants/global.js
@@ -4,8 +4,8 @@
 angular.module('jsApp')
 
   .constant("APP_PROPERTIES", {
-    "HOST_": "votalatua.estate",
-    "HOST": "localhost:8080",
+    "HOST": "votalatua.estate",
+    "HOST_": "localhost:8080",
     "PAGE": 1,
     "COUNT": 5
   });
